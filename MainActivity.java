fdb3b88133002f31080a0437de0bcf10bb6434a8
Dave Greaves <davidmgreaves@gmail.com>
2018-03-05 21:49:48 +1000

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.greaves.david.materical_cardview_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
